7f5fb1a1e2e092ab24bab1590eaec6bdb5cebaa4
Mario Krenn <46604014+MarioKrenn6240@users.noreply.github.com>
2019-09-02 16:21:14 -0400

    Update selfies_example.py

--- examples/selfies_example.py
+++ examples/selfies_example.py
@@ -1,7 +1,7 @@
 # =============================================================================
 # 
 # SELFIES: a robust representation of semantically constrained graphs with an example application in chemistry
-#               v0.2.0, 02. September 2019
+#               v0.2.1, 02. September 2019
 # by Mario Krenn, Florian Haese, AkshatKuman Nigam, Pascal Friederich, Alan Aspuru-Guzik
 #
 # SELFIES (SELF-referencIng Embedded Strings) is a general-purpose, sequence-based,
