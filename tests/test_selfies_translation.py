68e668fc8df6cc062d36c1e7c110fd8cb9c8ccab
alstonlo <40709307+alstonlo@users.noreply.github.com>
2020-06-03 21:08:41 -0400

    Integrating tests with pytest

--- tests/test_selfies_translation.py
+++ tests/test_selfies_translation.py
@@ -1,10 +1,12 @@
 import faulthandler
+
 faulthandler.enable()
 
-from selfies import encoder, decoder
-from rdkit.Chem import MolFromSmiles, MolToSmiles
-import pandas as pd
+import pytest
 import random
+
+import selfies as sf
+from rdkit.Chem import MolFromSmiles, MolToSmiles
 
 test_path_list = [
     'test_sets/dataA_QM9.txt',
@@ -12,36 +14,64 @@ test_path_list = [
     'test_sets/dataJ_250k_rndm_zinc_drugs_clean.txt'
 ]
 
-def test_selfies_translation(test_path, sample_size):
+
+@pytest.mark.parametrize("test_paths", [test_path_list])
+def test_roundtrip_translation(test_paths, sample_size=1000):
+    """Tests <sample_size> random SMILES from each data set in <test_paths>;
+    encodes and then decodes the SMILES, and checks whether the decoded
+    SMILES corresponds to the same molecule as the input SMILES.
+    """
+
     error_list = []
 
+    for test_path in test_paths:
 
-    print ('Testing following dataset:' + test_path)
-    smi_df = pd.read_csv(test_path)
-    smi_df.columns = ['smiles']
-    smiles = smi_df['smiles']
-    # random.shuffle(smiles) # shuffle the SMILES such that new batch is always tested
+        with open(test_path, 'r') as test_file:
+            smiles_set = random.sample(test_file.readlines(), sample_size)
+            smiles_set = [line.rstrip() for line in smiles_set]
 
-    for i, smi in enumerate(smiles):
-        if i==sample_size+1:
-            break
+        for smiles in smiles_set:
 
-        encoded_selfies=encoder(smi)
-        decoded_smiles=decoder(encoded_selfies, N_restrict=False)
-        s1=MolToSmiles(MolFromSmiles(smi))
-        s2=MolToSmiles(MolFromSmiles(decoded_smiles))
+            decoded_smiles = sf.decoder(sf.encoder(smiles), N_restrict=False)
 
-        if s1==s2:
-            # print('SMILES are the same: ',s1)
-            continue
-        else:
-            print('ERROR: SMILES are different: ' + s1 + ' - ' + s2)
-            error_list.append(str(i) + ': ERROR!: ' + s1)
+            can_input = MolToSmiles(MolFromSmiles(smiles))
+            can_output = MolToSmiles(MolFromSmiles(decoded_smiles))
 
-    with open("error_list.txt", "w") as file:
+            if can_input != can_output:
+                error_list.append((smiles, decoded_smiles,
+                                   can_input, can_output))
+
+    with open("error_list.csv", "w") as error_log:
+        error_log.write("In, Out, Canonical In, Canonical Out")
         for error in error_list:
-            file.write(error + "\n")
+            error_log.write(','.join(error) + "\n")
+
+    assert len(error_list) == 0
+
+
+def test_random_selfies_decoder():
+    """Passes random strings built from the SELFIES alphabet to the decoder,
+    and uses RDKit to check whether the decoded SMILES are valid.
+    """
+    trials = 1000
+    max_len = 50
+    alphabet = sf.selfies_alphabet()
+
+    for i in range(trials):
+
+        # create random SELFIES and decode
+        rand_len = random.randint(1, max_len)
+        selfies = ''.join(random.choice(alphabet) for _ in range(rand_len))
+        smiles = sf.decoder(selfies)
+
+        # check if SMILES is valid
+        try:
+            is_valid = MolFromSmiles(smiles, sanitize=True) is not None
+        except Exception:
+            is_valid = False
+
+        assert is_valid, f"Invalid SMILES {smiles} decoded from {selfies}"
+
 
 if __name__ == '__main__':
-    for test_path in test_path_list:
-        test_selfies_translation(test_path, 1000)
+    pytest.main()
