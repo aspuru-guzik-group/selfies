57828e59da749ebc6c2bd093f168f13181a57107
alstonlo <40709307+alstonlo@users.noreply.github.com>
2020-06-21 22:22:57 -0400

    Delete s.py

--- tests/s.py
+++
@@ -1,5 +0,0 @@
-from rdkit.Chem import MolFromSmiles
-import selfies
-
-print(MolFromSmiles("O=C(O)c1c(-c2cnc(-c3nc4ccccc4s3)nn2)csc1-c1cnc(-c2nc3ccccc3s2)nn1"))
-print(selfies.encoder("O=C(O)c1c(-c2cnc(-c3nc4ccccc4s3)nn2)csc1-c1cnc(-c2nc3ccccc3s2)nn1"))
