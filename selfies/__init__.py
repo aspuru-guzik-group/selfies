2647a1fe6761d0efafdd22cc6f7a0b292f73df1c
alstonlo <40709307+alstonlo@users.noreply.github.com>
2021-08-30 00:27:14 -0400

    commenting

--- selfies/__init__.py
+++ selfies/__init__.py
@@ -1,3 +1,30 @@
+#!/usr/bin/env python
+
+"""
+SELFIES: a robust representation of semantically constrained graphs with an
+         example application in chemistry.
+
+SELFIES (SELF-referencIng Embedded Strings) is a general-purpose,
+sequence-based, robust representation of semantically constrained graphs.
+It is based on a Chomsky type-2 grammar, augmented with two self-referencing
+functions. A main objective is to use SELFIES as direct input into machine
+learning models, in particular in generative models, for the generation of
+outputs with high validity.
+
+The code presented here is a concrete application of SELFIES in chemistry, for
+the robust representation of molecules.
+
+    Typical usage example:
+        import selfies as sf
+
+        benzene = "C1=CC=CC=C1"
+        benzene_selfies = sf.encoder(benzene)
+        benzene_smiles = sf.decoder(benzene_selfies)
+
+For comments, bug reports or feature ideas, please send an email to
+mario.krenn@utoronto.ca and alan@aspuru.com.
+"""
+
 __version__ = "1.0.3"
 
 __all__ = [
