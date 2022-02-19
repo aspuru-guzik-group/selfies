9d3cce759adbfe2b0dac63c1edc6df12313eec14
Jannis Born <jab@zurich.ibm.com>
2022-02-19 22:47:06 +0100

    chore: fix CI pipeline

--- tests/test_selfies.py
+++ tests/test_selfies.py
@@ -154,7 +154,8 @@ def test_encoder_attribution():
     # check that Cl lined up
     for i, ta in enumerate(am):
         if ta[1]:
-            assert indices[i] == ta[1][0][0], f'found {ta[1]}; should be {indices[i]}'
+            assert indices[i] == ta[1][0][0], \
+                f'found {ta[1]}; should be {indices[i]}'
         if ta[0] == '[Cl]':
             for i, v in ta[1]:
                 if v == 'Cl':
