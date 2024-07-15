37c914b5a268c86a2ab626d30f7d9c3fed498873
Mario Krenn <46604014+MarioKrenn6240@users.noreply.github.com>
2024-07-15 02:04:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="selfies",
-    version="2.1.1",
+    version="2.1.2",
     author="Mario Krenn, Alston Lo, and many other contributors",
     author_email="mario.krenn@utoronto.ca, alan@aspuru.com",
     description="SELFIES (SELF-referencIng Embedded Strings) is a "
